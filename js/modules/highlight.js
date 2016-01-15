d5034f3c345cf2b8e0f427e72ed82b49ca32b99d
Jamie Graham <jamie@domain7.com>
2016-01-15 12:02:17 -0800

    Updated highlighting

--- js/modules/highlight.js
+++ js/modules/highlight.js
@@ -21,34 +21,18 @@ function removeExistingHighlight() {
 
 function insertHighlightAtCaret(html) {
   var sel, range;
-  if (window.getSelection) {
-    // IE9 and non-IE
-    sel = window.getSelection();
-    if (sel.getRangeAt && sel.rangeCount) {
-      range = sel.getRangeAt(0);
-      range.deleteContents();
 
-      // Range.createContextualFragment() would be useful here but is
-      // non-standard and not supported in all browsers (IE9, for one)
-      var el = document.createElement("div");
-      el.innerHTML = html;
-      var frag = document.createDocumentFragment(), node, lastNode;
-      while ( (node = el.firstChild) ) {
-        lastNode = frag.appendChild(node);
-      }
-      range.insertNode(frag);
-      
-      // Preserve the selection
-      if (lastNode) {
-        range = range.cloneRange();
-        range.setStartAfter(lastNode);
-        range.collapse(true);
-        sel.removeAllRanges();
-        sel.addRange(range);
-      }
+  sel = window.getSelection();
+  if (sel.getRangeAt && sel.rangeCount) {
+    range = sel.getRangeAt(0);
+    range.deleteContents();
+
+    var el = document.createElement("div");
+    el.innerHTML = html;
+    var frag = document.createDocumentFragment(), node, lastNode;
+    while ( (node = el.firstChild) ) {
+      lastNode = frag.appendChild(node);
     }
-  } else if (document.selection && document.selection.type != "Control") {
-    // IE < 9
-    document.selection.createRange().pasteHTML(html);
+    range.insertNode(frag);   
   }
 }
